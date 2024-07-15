59eada7b50a7390362d682bf9964cac6b6d8846a
Oleksandr <gayrsuk@gmail.com>
2024-07-16 00:23:39 +0300

    added implementation removed last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'node:fs';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+
+    contacts.pop();
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    console.log('Last contact has been removed');
+  } catch (error) {
+    console.error(`Error removed last contact: ${error}`);
+  }
+};
 
 removeLastContact();
