d09f12f89716d43f17ad466d483937b3af57d7d2
Oleksandr <gayrsuk@gmail.com>
2024-07-15 23:48:44 +0300

    added implementation addOneContact func

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,25 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'node:fs';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    const newContact = createFakeContact();
+
+    const updateContacts = [...contacts, newContact];
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updateContacts, null, 2),
+      'utf8',
+    );
+
+    console.log(`New contact have been added`);
+  } catch (error) {
+    console.error(`Error added contact: ${error}`);
+  }
+};
 
 addOneContact();
