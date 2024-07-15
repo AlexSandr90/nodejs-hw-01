98d233c5ae41b41358f06fda69fa6962d3968457
Oleksandr <gayrsuk@gmail.com>
2024-07-16 00:11:26 +0300

    added implementation getAllContacts func

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'node:fs';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error read contacts: ${error}`);
+  }
+};
 
 console.log(await getAllContacts());
