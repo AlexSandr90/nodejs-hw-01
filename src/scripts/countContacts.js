a3af1a33603b5ae8857894f5f342e3ca592bd8f6
Oleksandr <gayrsuk@gmail.com>
2024-07-16 00:12:27 +0300

    added implementation countContacts func

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'node:fs';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+
+    return JSON.parse(data)?.length;
+  } catch (error) {
+    console.error(`Error read contacts: ${error}`);
+  }
+};
 
 console.log(await countContacts());
